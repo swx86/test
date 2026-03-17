150e153d285473fd740c68ed4e5d8968252b8bfa
swx86 <370585769@qq.com>
2026-03-17 10:32:13 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello, this is a Python project created in Trae!")
+    print("You can now use GitHub MCP to push this to your repository.")
+
+if __name__ == "__main__":
+    main()
